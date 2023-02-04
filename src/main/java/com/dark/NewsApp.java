173972ce5a07f671e6167b0a8651650088a5a6c1
Renat <renat379@gmail.com>
2023-02-04 19:05:57 +0600

    initial commit

---
+++ src/main/java/com/dark/NewsApp.java
@@ -0,0 +1,9 @@
+package com.dark;
+
+import org.springframework.boot.SpringApplication;
+
+public class NewsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NewsApp.class, args);
+    }
+}
