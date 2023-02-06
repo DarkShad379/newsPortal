d8db181a964f0daea7a24766980e134ed054907d
Renat <renat379@gmail.com>
2023-02-06 17:33:09 +0600

    add: NewsController first implementation

---
+++ src/main/java/com/dark/news/controller/NewsController.java
@@ -0,0 +1,68 @@
+package com.dark.news.controller;
+
+import com.dark.news.service.NewsService;
+import com.dark.news.service.model.NewsModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequestMapping("news")
+public class NewsController {
+    private final NewsService newsService;
+
+    public NewsController(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
+    @GetMapping
+    public ModelAndView getAllNews(ModelMap model) {
+        List<NewsModel> newsList = newsService.getAllNews();
+        model.addAllAttributes(newsList);
+        return new ModelAndView("/news", model);
+    }
+
+    @GetMapping("edit/{id}")
+    public ModelAndView getNewsForEditById(@PathVariable long id, ModelMap model) {
+        NewsModel selected = newsService.getNews(id);
+        model.addAttribute(selected);
+        return new ModelAndView("/news/edit", model);
+    }
+
+    @GetMapping("edit/{id}")
+    public ModelAndView editNewsById(BindingResult bindingResult, @Valid @ModelAttribute("selectedNews") NewsModel selectedNews, ModelMap model) {
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("/news/edit/", model);
+        } else {
+            newsService.updateNews(selectedNews);
+            return new ModelAndView("redirect:..", model);
+        }
+    }
+
+    @PostMapping("remove")
+    public ModelAndView removeNewsList(@ModelAttribute("selectedNewsList") List<NewsModel> selectedNewsList) {
+        newsService.removeListOfNews(selectedNewsList);
+        return new ModelAndView("/news");
+    }
+
+    @GetMapping("/add")
+    public ModelAndView getAddNewsPage(ModelMap model) {
+        model.addAttribute("newNewsModel", new NewsModel());
+        return new ModelAndView("/add", model);
+    }
+
+    @Validated
+    @PostMapping("/add")
+    public ModelAndView addNews(@Valid @ModelAttribute("newNewsModel") NewsModel newNewsModel, BindingResult bindingResult) {
+        if (!bindingResult.hasErrors()) {
+            newsService.updateNews(newNewsModel);
+            return new ModelAndView("redirect:");
+        } else return new ModelAndView("news/add");
+    }
+}
