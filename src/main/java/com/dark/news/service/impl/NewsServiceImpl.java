9c7a18901d117a74f11b4676321ca025ba08109c
Renat <renat379@gmail.com>
2023-02-06 17:34:19 +0600

    add: Validating annotation for NewsModel fields make newsRepository private final in NewsServiceImpl

--- src/main/java/com/dark/news/service/impl/NewsServiceImpl.java
+++ src/main/java/com/dark/news/service/impl/NewsServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class NewsServiceImpl implements NewsService {
-    NewsRepository newsRepository;
+    private final NewsRepository newsRepository;
 
     public NewsServiceImpl(NewsRepository newsRepository) {
         this.newsRepository = newsRepository;
--- src/main/java/com/dark/news/service/model/NewsModel.java
+++ src/main/java/com/dark/news/service/model/NewsModel.java
@@ -4,15 +4,26 @@ import com.dark.news.database.entity.news.NewsEntity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 public class NewsModel {
+    @NotNull
     private long id;
+    @NotNull
+    @NotEmpty
     private String title;
+    @NotNull
+    @NotEmpty
     private LocalDateTime dateTime;
+    @NotNull
+    @NotEmpty
     private String brief;
+    @NotNull
+    @NotEmpty
     private String content;
 
     public NewsModel(NewsEntity newsEntity) {
